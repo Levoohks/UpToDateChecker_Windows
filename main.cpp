bade5aed7c92be0f5df28f101ec56463c2158f82
Levoohks <Levoohks@gmail.com>
2022-04-01 22:29:44 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void check_windows_version()
+{
+    OSVERSIONINFOEX osvi;
+    ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
+    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+
+    if (!GetVersionEx((OSVERSIONINFO*)&osvi))
+    {
+        return;
+    }
+
+    if (osvi.dwMajorVersion > 6 || (osvi.dwMajorVersion == 6 && osvi.dwMinorVersion >= 2))
+    {
+        cout << "Windows version is up to date" << endl;
+    }
+    else
+    {
+        cout << "Windows version is not up to date" << endl;
+    }
+}
+
+int main()
+{
+    check_windows_version();
+    return 0;
+}
